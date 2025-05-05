3353d7f0250fc8a8c898138ce1e3524aa164d452
Kosto1221 <ostolany@gamil.com>
2025-05-05 20:04:15 +0900

    #8.9 Edit Profile

---
+++ app/features/users/mutations.ts
@@ -0,0 +1,27 @@
+import pkg from "@supabase/supabase-js";
+import type { Database } from "~/supa-client";
+
+export const updateUser = async (
+  client: pkg.SupabaseClient<Database>,
+  {
+    id,
+    name,
+    role,
+    headline,
+    bio,
+  }: {
+    id: string;
+    name: string;
+    role: "developer" | "designer" | "marketer" | "founder" | "product-manager";
+    headline: string;
+    bio: string;
+  }
+) => {
+  const { error } = await client
+    .from("profiles")
+    .update({ name, role, headline, bio })
+    .eq("profile_id", id);
+  if (error) {
+    throw error;
+  }
+};
