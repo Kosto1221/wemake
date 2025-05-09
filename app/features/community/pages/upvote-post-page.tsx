e5d9bdb0e617572585ee11dadf3e2db15a7bfa47
Kosto1221 <ostolany@gamil.com>
2025-05-09 19:48:30 +0900

    #9.3 Optimistic UI

--- app/features/community/pages/upvote-post-page.tsx
+++ app/features/community/pages/upvote-post-page.tsx
@@ -1,9 +1,15 @@
+import { getLoggedInUserId } from "~/features/users/queries";
 import type { Route } from "./+types/upvote-post-page";
+import { toggleUpvote } from "../mutations";
+import { makeSSRClient } from "~/supa-client";
 
 export const action = async ({ request, params }: Route.ActionArgs) => {
-  const formData = await request.formData();
-  const postId = formData.get("postId");
-  console.log(postId, "fetcher submit says");
+  if (request.method !== "POST") {
+    throw new Response("Method not allowed", { status: 405 });
+  }
+  const { client } = await makeSSRClient(request);
+  const userId = await getLoggedInUserId(client);
+  await toggleUpvote(client, { postId: params.postId, userId });
   return {
     ok: true,
   };
