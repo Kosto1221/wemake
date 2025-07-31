440020f9bfd7e33528c446781c773b667b47786f
Kosto1221 <ostolany@gamil.com>
2025-07-31 11:40:48 +0900

    #4.7 Team Table

---
+++ app/features/teams/constants.ts
@@ -0,0 +1,8 @@
+import { m } from "node_modules/react-router/dist/development/components-DzqPLVI1.mjs";
+
+export const PRODUCT_STAGES = [
+  { label: "Idea", value: "idea" },
+  { label: "Prototype", value: "prototype" },
+  { label: "MVP", value: "mvp" },
+  { label: "Product", value: "product" },
+];
